5d7f94505b3e1577b8b67615e395f5483f399fbb
debrasnicolas <nicodebras17@gmail.com>
2023-01-04 14:27:53 +0100

    refactor driving

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceUpdateServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceUpdateServiceTest.java
@@ -5,6 +5,7 @@ import fr.esgi.cleancode.exception.ResourceNotFoundException;
 import fr.esgi.cleancode.model.DrivingLicence;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,19 +25,20 @@ public class DrivingLicenceUpdateServiceTest {
 
     @Mock
     private InMemoryDatabase database ;
-    private DrivingLicence validDrivingLicence ;
+    private DrivingLicence drivingLicence ;
     private final UUID drivingLicenceId = DrivingLicenceIdGenerationService.generateNewDrivingLicenceId();
 
     @BeforeEach
     public void init_Valid_Driving_Licence () {
-        validDrivingLicence = DrivingLicence.builder()
+        drivingLicence = DrivingLicence.builder()
                 .id(drivingLicenceId)
                 .driverSocialSecurityNumber("123456789012345")
                 .build();
     }
 
     @Test
-    public void should_throw_ff_driving_licence_not_found () {
+    @DisplayName("should throw ResourceNotFoundException if driving licence not found")
+    public void should_throw_if_driving_licence_not_found () {
         when(database.findById(drivingLicenceId)).thenReturn(Optional.empty());
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
             service.RemovePoints(drivingLicenceId, 6);
@@ -44,13 +46,12 @@ public class DrivingLicenceUpdateServiceTest {
     }
 
     @Test
+    @DisplayName("should not remove points if driving licence have 0 point")
     public void should_Not_Remove_Points_Below_Zero () {
-        DrivingLicence drivingLicenceWithZeroPoints = validDrivingLicence.withAvailablePoints(0);
+        DrivingLicence drivingLicenceWithZeroPoints = drivingLicence.withAvailablePoints(0);
 
-        when(database.findById(drivingLicenceId)).thenReturn(Optional.ofNullable(validDrivingLicence));
+        when(database.findById(drivingLicenceId)).thenReturn(Optional.ofNullable(drivingLicence));
         when(database.save(drivingLicenceId, drivingLicenceWithZeroPoints)).thenReturn(drivingLicenceWithZeroPoints);
-
-        Assertions.assertEquals(12, validDrivingLicence.getAvailablePoints());
 
         final DrivingLicence updatedDrivingLicence = service.RemovePoints(drivingLicenceId, 13);
         Assertions.assertEquals(0, updatedDrivingLicence.getAvailablePoints());
@@ -58,21 +59,20 @@ public class DrivingLicenceUpdateServiceTest {
 
     @Test
     public void should_Update_Given_Number_Of_Points () {
-        DrivingLicence drivingLicenceWithSixPoints = validDrivingLicence.withAvailablePoints(6);
+        DrivingLicence drivingLicenceWithSixPoints = drivingLicence.withAvailablePoints(6);
 
-        when(database.findById(drivingLicenceId)).thenReturn(Optional.ofNullable(validDrivingLicence));
+        when(database.findById(drivingLicenceId)).thenReturn(Optional.ofNullable(drivingLicence));
         when(database.save(drivingLicenceId, drivingLicenceWithSixPoints)).thenReturn(drivingLicenceWithSixPoints);
 
-        Assertions.assertEquals(12, validDrivingLicence.getAvailablePoints());
         final DrivingLicence updatedDrivingLicence = service.RemovePoints(drivingLicenceId, 6) ;
         Assertions.assertEquals(drivingLicenceWithSixPoints.getAvailablePoints(), updatedDrivingLicence.getAvailablePoints());
     }
 
     @Test
     public void should_Update_And_Return_Driving_Licence () {
-        DrivingLicence drivingLicenceWithSixPoints = validDrivingLicence.withAvailablePoints(6);
+        DrivingLicence drivingLicenceWithSixPoints = drivingLicence.withAvailablePoints(6);
 
-        when(database.findById(drivingLicenceId)).thenReturn(Optional.ofNullable(validDrivingLicence));
+        when(database.findById(drivingLicenceId)).thenReturn(Optional.ofNullable(drivingLicence));
         when(database.save(drivingLicenceId, drivingLicenceWithSixPoints)).thenReturn(drivingLicenceWithSixPoints);
 
         final DrivingLicence updatedDrivingLicence = service.RemovePoints(drivingLicenceId, 6) ;
