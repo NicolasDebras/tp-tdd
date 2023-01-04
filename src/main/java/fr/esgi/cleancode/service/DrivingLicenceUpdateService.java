1ea1f9dae1215be6cdb77564abadf80eb1883970
debrasnicolas <nicodebras17@gmail.com>
2023-01-04 10:39:02 +0100

    refactor ; change for better name remove -> update

---
+++ src/main/java/fr/esgi/cleancode/service/DrivingLicenceUpdateService.java
@@ -0,0 +1,13 @@
+package fr.esgi.cleancode.service;
+
+import fr.esgi.cleancode.model.DrivingLicence;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class DrivingLicenceUpdateService {
+
+    public DrivingLicence RemovePoints(UUID drivingLicenceId, Integer pointsTORemove) {
+        return null;
+    }
+}
