ed816c2d1468c0556c43d0ab2103c566ad953189
debrasnicolas <nicodebras17@gmail.com>
2023-01-04 10:49:51 +0100

    dev RemovePoints (test sucess)

--- src/main/java/fr/esgi/cleancode/service/DrivingLicenceUpdateService.java
+++ src/main/java/fr/esgi/cleancode/service/DrivingLicenceUpdateService.java
@@ -1,13 +1,32 @@
 package fr.esgi.cleancode.service;
 
+import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.exception.ResourceNotFoundException;
 import fr.esgi.cleancode.model.DrivingLicence;
+import lombok.RequiredArgsConstructor;
 
-import java.util.Optional;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 public class DrivingLicenceUpdateService {
 
-    public DrivingLicence RemovePoints(UUID drivingLicenceId, Integer pointsTORemove) {
-        return null;
+    private final InMemoryDatabase database;
+
+    public DrivingLicence RemovePoints(UUID drivingLicenceId, Integer pointsTORemove)  throws ResourceNotFoundException {
+        // Find the driving license to update in the database
+        var drivingLicenceToUpdate = this.database.findById(drivingLicenceId) ;
+        // If the driving license is not found, throw a ResourceNotFoundException
+        if (drivingLicenceToUpdate.isEmpty()) {
+            throw new ResourceNotFoundException("Driving licence with id " + drivingLicenceId.toString() + " not found");
+        }
+        // Get the updated driving license object
+        DrivingLicence updatedDrivingLicence = drivingLicenceToUpdate.get();
+        // Calculate the updated number of points, making sure it doesn't go below zero
+        final var updatedPoints = Math.max(updatedDrivingLicence.getAvailablePoints() - pointsTORemove, 0);
+        // Update the driving license object with the new number of points
+        updatedDrivingLicence = updatedDrivingLicence.withAvailablePoints(updatedPoints);
+        // Save the updated driving license object to the database and return it
+        return database.save(drivingLicenceId, updatedDrivingLicence);
     }
+
 }
